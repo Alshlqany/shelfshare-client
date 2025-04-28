ee1131d38c05c12cb8c750f47e0703f128d512b2
Alshlqany <moalshlkany@gmail.com>
2025-04-28 17:19:59 +0300

    fix: adjust padding in FormContainer for improved layout consistency

--- src/components/Auth/FormContainer.jsx
+++ src/components/Auth/FormContainer.jsx
@@ -1,6 +1,6 @@
 const FormContainer = ({ title, children }) => {
   return (
-    <div className="p-20  w-full h-screen flex flex-col justify-center md:bg-white lg:p-30">
+    <div className="p-10 md:p-20 w-full h-screen flex flex-col justify-center md:bg-white lg:p-30">
       <h1 className="text-3xl font-bold text-[#2C9DB7] mb-2 md:mb-6 ">
         {title}
       </h1>
